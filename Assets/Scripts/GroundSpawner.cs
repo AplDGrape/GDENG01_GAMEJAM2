88c379052a06e8a02f7c2e131ce0738044d7bafa
AplDGrape <61457409+AplDGrape@users.noreply.github.com>
2023-11-12 17:30:16 +0800

    Added obstacle lists, player speed increase, & player death
    - Obstacle list can now take in multiple types of obstacles and different variety ("wow" - Owen Wilson)
    - Player speed increases now over time (every 10 second by 10f speed)
    - Once player hits an obstacle, they restart the scene from the start

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -7,64 +7,42 @@ namespace Wave
 {
     public class GroundSpawner : MonoBehaviour
     {
+        GroundTile groundtile;
+
         [SerializeField] private int tileStartCount = 5;
-        //[SerializeField] private int minStraightTiles = 3;
-        //[SerializeField] private int maxStraightTiles = 10;
 
-        [SerializeField] private GameObject startingTile;
-        [SerializeField] List<GameObject> obstacles;
-
-        private Vector3 currentTileLocation = Vector3.zero;
-        private Vector3 currentTileDirection = Vector3.forward;
-        private GameObject prevTile;
-
-        private List<GameObject> currentTiles;
-        private List<GameObject> currentObstacles;
-
-        public GameObject groundTile;
+        [SerializeField] GameObject groundTile;
         Vector3 nextSpawnPoint;
+
+        public void SpawnTile(/*bool DelayObstacles*/)
+        {
+            GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
+            nextSpawnPoint = temp.transform.GetChild(1).transform.position;
+
+            /*if (DelayObstacles)
+            {
+                temp.GetComponent<GroundTile>().SpawnObstacle();
+            }*/
+        }
 
         // Start is called before the first frame update
         void Start()
         {
-            currentTiles = new List<GameObject>();
-            currentObstacles = new List<GameObject>();
 
             //To give random unique number lel
-            Random.InitState(System.DateTime.Now.Millisecond);
+            //Random.InitState(System.DateTime.Now.Millisecond);
 
             #region Handles amount of platform to spawn
             for (int i = 0; i < tileStartCount; i++)
             {
-                SpawnTile(/*startingTile.GetComponent<Tile>(), false*/);
+                /*if (i < 1) 
+                    SpawnTile(false);
+                else
+                    SpawnTile(true);*/
+
+                SpawnTile();
             }
             #endregion
         }
-
-        public void SpawnTile(/*Tile tile, bool spawnObstacle*/)
-        {
-            //prevTile = GameObject.Instantiate(tile.gameObject, currentTileLocation, Quaternion.identity);
-            //currentTiles.Add(prevTile);
-            GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
-            //currentTileLocation += Vector3.Scale(prevTile.GetComponent<Renderer>().bounds.size, currentTileDirection);
-            nextSpawnPoint = temp.transform.GetChild(1).transform.position;
-
-            //if (SpawnObstacle) SpawnObstacle();
-        }
-
-        //private void SpawnObstacle()
-        //{
-        //    if (Random.value > 0.2f) return;
-
-        //    GameObject obstaclePrefab = SelectRandomGameObjectFromList(obstacles);
-        //    GameObject obstacle = Instantiate(obstaclePrefab, currentTileLocation, Quaternion.identity);
-        //    currentObstacles.Add(obstacle);
-        //}
-
-        //private GameObject SelectRandomGameObjectFromList(List<GameObject> list)
-        //{
-        //    if (list.Count == 0) return null;
-        //    return list[SelectRandomGameObjectFromList().Range(0, list.Count)];
-        //}
     }
 }
--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -9,34 +9,40 @@ namespace Wave
     {
         GroundSpawner groundSpawner;
 
+        [SerializeField] List<GameObject> obstacles;
+        private List<GameObject> currentObstacles;
+
         // Start is called before the first frame update
         void Start()
         {
+            currentObstacles = new List<GameObject>();
+
             groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
             SpawnObstacle();
         }
 
         void OnTriggerExit(Collider other)
         {
-            groundSpawner.SpawnTile();
-            Destroy(gameObject, 3);
+            groundSpawner.SpawnTile(/*true*/);
+            Destroy(gameObject, 2);
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        public GameObject obstaclePrefab;
-
-        void SpawnObstacle()
+        public void SpawnObstacle()
         {
             int obstacleSpawnIndex = Random.Range(2, 5);
             Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
 
-            Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+            //Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+            GameObject obstaclePrefab = SelectRandomGameObjectFromList(obstacles);
+            GameObject obstacle = Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+            currentObstacles.Add(obstacle);
 
+        }
+
+        private GameObject SelectRandomGameObjectFromList(List<GameObject> list)
+        {
+            if (list.Count == 0) return null;
+            return list[Random.Range(0, list.Count)];
         }
     }
 }
